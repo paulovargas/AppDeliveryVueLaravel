e6373a37efec33d1659772109220c08cee57c267
Paulo Vargas <paulotomegomesdevargas@hotmail.com>
2023-08-31 21:32:42 -0300

    Login Modal

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Delivery</a>
+      <a class="navbar-brand" href="#">Seu Delivery</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -13,6 +13,11 @@ import { RouterLink, RouterView } from 'vue-router'
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <RouterLink class="nav-link" to="/">Home</RouterLink>
+          </li>
+          <li>
+            <button type="button" class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          Entrar
+          </button>
           </li>
           <li class="nav-item">
             <RouterLink class="nav-link" to="/sobre">Sobre</RouterLink>
@@ -27,7 +32,30 @@ import { RouterLink, RouterView } from 'vue-router'
       </div>
     </div>
   </nav>
-  
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Entre para fazer seu pedido !</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Email</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Seu email !">
+      </div>
+      <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Senha</label>
+      <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Sua senha!">
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Acessar!</button>
+      </div>
+    </div>
+  </div>
+</div>  
     <RouterView />
   </template>
 
